bd5eeba14c85c7b2baa7665dd7cacb0c6bd04a9b
coder-sadhin <codersadhin@gmail.com>
2024-08-24 18:26:08 +0600

    level order print

--- level_order_print.cpp
+++ level_order_print.cpp
@@ -13,7 +13,31 @@ public:
         this->right = NULL;
     }
 };
+void level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "Tree nai" << endl;
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ana
+        Node *f = q.front();
+        q.pop();
 
+        // 2. jabotiyo ja kaj ache
+        cout << f->val << " ";
+
+        // 3. tar children gulo ke rakha
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
 int main()
 {
     Node *root = new Node(10);
